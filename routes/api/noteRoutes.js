eb65379319a2a8b5ce0b919c0c4e3913629a1a1b
Yahvannel <yahvannellopez@gmail.com>
2024-06-16 11:36:26 -0500

    added router get and delete requests

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,16 +1,54 @@
 // const uniqid = require("uniqid");
 const router = require("express").Router()
+const { json } = require("express");
+const fs = require("fs")
+const path = require("path")
+const dbPath = path.join( __dirname,"../../db/db.json")
 // Routes
+
+// Get request
+router.get("", (req,res) => {
+let data = fs.readFileSync(dbPath, "utf8");
+
+res.json(JSON.parse(data));
+});
+
+// Post request
 router.post("/", (req,res) => {
+    console.log("POST ROUTE!!!!1")
+    let data = fs.readFileSync( dbPath, "utf8");
+    const dataJSON = JSON.parse(data);
+    console.log("====unparesed data below===== ")
+    console.log(data)
+    console.log('=====data json below')
+    console.log(dataJSON)
+    console.log(req.body)
     const newNote = {
         ...req.body,
-        id: uniqid(),
+        // id: uniqid(),
     };
-
-    let data = fs.readFileSync("./db/db.json", "utf8");
-
-    const dataJSON = JSON.parse(data);
+    dataJSON.push(newNote)
+    fs.writeFileSync(dbPath, JSON.stringify(dataJSON))
+    res.json("ADDED IT!")
 })
+
+// Delete request
+router.delete("//path to notee//", (req, res) => {
+    let data = fs.readFileSync(dbPath, "utf8");
+    const dataJSON = JSON.parse(data);
+
+    const newNotes = dataJSON.filter((note) => {
+        return note.id !== req.params.id;
+    });
+
+    fs.writeFile(dbPath, JSON.stringify(newNotes), (err, text) => {
+        if (err) {
+            crossOriginIsolated.error(err);
+            return;
+        }
+    });
+    res.json(newNotes);
+});
 
 
 module.exports = router;  
